111b04db48dd1a0a051d6d7bd2176c3cbc190209
mhikeo <mhjkc@Mhike-Ocampos-iMac.local>
2014-08-21 06:57:20 +0800

    Initial code

---
+++ SRMPracticeTimer/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SRMPracticeTimer
+//
+//  Created by mhjkc on 20/8/14.
+//  Copyright (c) 2014 mhykol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func gotoMain(_: AnyObject) {
+        
+    }
+
+}
+
